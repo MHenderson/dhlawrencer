1b395382d2da55d78666bb11b93786236936d702
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-04-27 20:06:19 +0100

    Documentation for all novel data sets.

---
+++ R/novels.R
@@ -0,0 +1,75 @@
+#' D. H. Lawrence's Novels
+#' 
+#' This package contains the complete text of D. H. Lawrence's novels.
+#' @docType package
+#' @name dhlawrencer
+#' @aliases dhlawrencer dhlawrencer-package
+NULL
+
+#' The text of D. H. Lawrence's novel "Aaron's Rod"
+#'
+#' A dataset containing the text of D. H. Lawrence's 1922 novel "Aaron's 
+#' Rod". The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/4520}
+#' @format A character vector with 13402 elements
+"aaronsrod"
+
+#' The text of D. H. Lawrence's novel "The Lost Girl"
+#'
+#' A dataset containing the text of D. H. Lawrence's 1920 novel "The Lost
+#' Girl". The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/23727}
+#' @format A character vector with 15742 elements
+"lostgirl"
+
+#' The text of D. H. Lawrence's novel "The White Peacock"
+#'
+#' A dataset containing the text of D. H. Lawrence's 1911 novel "The White
+#' Peacock". The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/38561}
+#' @format A character vector with 14626 elements
+"peacock"
+
+#' The text of D. H. Lawrence's novel "The Rainbow"
+#'
+#' A dataset containing the text of D. H. Lawrence's 1915 novel "The Rainbow".
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each. 
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/28948}
+#' @format A character vector with 20962 elements
+"rainbow"
+
+#' The text of D. H. Lawrence's novel "Sons and Lovers"
+#'
+#' A dataset containing the text of D. H. Lawrence's novel "Sons and
+#' Lovers" published in 1913. The UTF-8 plain text was sourced from
+#' Project Gutenberg and is divided into elements of up to about 70 characters
+#' each. (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/217}
+#' @format A character vector with 18214 elements
+"sonsandlovers"
+
+#' The text of D. H. Lawrence's novel "Women in Love"
+#'
+#' A dataset containing the text of D. H. Lawrence's novel "Women in Love",
+#' published in 1920.
+#' The UTF-8 plain text was sourced from Project Gutenberg
+#' and is divided into elements of up to about 70 characters each.
+#' (Some elements are blank.)
+#'
+#' @source \url{http://www.gutenberg.org/ebooks/4240}
+#' @format A character vector with 20157 elements
+"womeninlove"
+
