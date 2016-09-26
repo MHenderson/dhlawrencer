550462d84b75ddff380791a5726a031969d47604
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-09-26 19:29:07 +0100

    Use gutenbergr for "The White Peacock".

--- data-raw/peacock.R
+++ data-raw/peacock.R
@@ -1,9 +1,5 @@
-begin_skip <- 131
-end_skip <- 391
-book_url <- "http://www.gutenberg.org/files/38561/38561-0.txt"
+library(gutenbergr)
 
-peacock <- readr::read_lines(book_url, skip = begin_skip)
-peacock <- peacock[1:(length(peacock) - end_skip)]
-peacock <- peacock[!is.na(peacock)]
+peacock <- gutenberg_download(38561-0)$text
 
 devtools::use_data(peacock, overwrite = TRUE)
