e2717b8a5f9f4aa2feee41d679db7244914b6a65
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-09-26 19:29:30 +0100

    Use gutenbergr for "The Rainbow".

--- data-raw/rainbow.R
+++ data-raw/rainbow.R
@@ -1,10 +1,5 @@
-begin_skip <- 94
-end_skip <- 377
+library(gutenbergr)
 
-book_url <- "http://www.gutenberg.org/cache/epub/28948/pg28948.txt"
-
-rainbow <- readr::read_lines(book_url, skip = begin_skip)
-rainbow <- rainbow[1:(length(rainbow) - end_skip)]
-rainbow <- rainbow[!is.na(rainbow)]
+rainbow <- gutenberg_download(28948)$text
 
 devtools::use_data(rainbow, overwrite = TRUE)
