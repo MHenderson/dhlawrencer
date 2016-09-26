d237166fe4ef8b89bc3dd2536caac66c6c53c9e5
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-09-26 19:18:28 +0100

    Use gutenbergr for "Aaron's Rod" data.

--- data-raw/aaronsrod.R
+++ data-raw/aaronsrod.R
@@ -1,9 +1,5 @@
-begin_skip <- 29
-end_skip <- 367
-book_url <- "http://www.gutenberg.org/cache/epub/4520/pg4520.txt"
+library(gutenbergr)
 
-aaronsrod <- readr::read_lines(book_url, skip = begin_skip)
-aaronsrod <- aaronsrod[1:(length(aaronsrod) - end_skip)]
-aaronsrod <- aaronsrod[!is.na(aaronsrod)]
+aaronsrod <- gutenberg_download(4520)$text
 
 devtools::use_data(aaronsrod, overwrite = TRUE)
