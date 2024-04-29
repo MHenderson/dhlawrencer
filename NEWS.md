81213cd9e3f27a8ac450f0bcdc722b40695a12a2
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-04-29 08:58:43 +0100

    Prepare next release (v0.1.1) (#2)
    * Remove Travis config.
    
    * Remove README image download script.
    
    * Update man pages.
    
    * Add a NEWS.md.
    
    * Bump version number.

---
+++ NEWS.md
@@ -0,0 +1,6 @@
+# dhlawrencer (development version)
+
+# dhlawrencer (v0.1.1)
+
+* Remove Travis config.
+* Remove download script for README image.
--- lawrence_image.R
+++
@@ -1,8 +0,0 @@
-library(magick)
-library(tidyverse)
-
-lawrence_url <- "https://upload.wikimedia.org/wikipedia/commons/6/68/D_H_Lawrence_1915.jpg"
-
-image_read(lawrence_url) %>%
-  image_crop("200x200+85+90") %>%
-  image_write("lawrence.jpg")
